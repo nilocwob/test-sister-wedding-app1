62f34bc6748ae1bea1dfc4fe648873efa05fceb9
Colin <colinbowden.work@gmail.com>
2023-06-25 10:53:10 -0700

    Update for simple email method

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import os
+
 from flask import Flask, render_template, request, flash
+import smtplib
 
 app = Flask(__name__)
 app.secret_key = "manbearpigMUD_1234456"
@@ -11,5 +14,21 @@ def index():
 
 @app.route("/choose_gift", methods=["POST", "GET"])
 def submit():
-  flash("You have requested the following gift: " + str(request.form['gift_type']))
+  message = ""
+  if request.form['gift_type'] != "Other":
+    message = str(request.form['gift_type'])
+  else:
+    message = str(request.form['gift_type']) + " - " + str(request.form['other_gift'])
+  flash("You have requested the following gift: " + message)
+
+  email = "cabvids@gmail.com"
+  from_addr = os.environ['FROM_EMAIL']
+
+  email_msg = "Request sent to Colin! Please give a gift of " + message
+
+  server = smtplib.SMTP("smtp.gmail.com", 587)
+  server.starttls()
+  server.login(from_addr, os.environ['EMAIL_PASSWORD'])
+  server.sendmail(from_addr, email, email_msg)
+
   return render_template("index.html")
